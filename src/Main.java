f31edda18f3f02a699bdcd8fe920aca020a568e7
Natali Dubrovina <natalka118@yandex.ru>
2024-08-11 13:47:36 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println(bonus);
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+    }
+}
